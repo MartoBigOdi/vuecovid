c90efe2ef6ac89bb0d877ed60b161940954a64dd
Marto Vasc <martin.vasconcelo@gmail.com>
2021-04-29 23:54:18 -0300

    Empezando con los componentes de datos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,48 @@
 <template>
-  <div class="home">
-   Hello MUNDO!
-  </div>
+  <main v-if ="!loading">
+    Datos
+  </main>
+
+  <main class="flex flex-col align-centrer justify-center text-center" v-else>
+      <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Datos en vivo:
+      </div>
+      <img :src="loadingImage" class="w-24 m-auto" alt="" />
+  </main>
+
 </template>
 
 <script>
 export default {
   name: 'Home',
   components: {},
-  created(){
-    console.log(123)
+  data(){
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/lavadomanos.webp')
+
+    }
   },
+  methods:{
+
+   async fetchCovidData() {
+     const res = await fetch('https://api.covid19api.com/summary');
+     const data = res.json();
+     return data;
+     }
+   },
+
+  async created(){
+    const data = this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+
 }
 </script>
